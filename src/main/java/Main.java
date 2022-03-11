79ad275b003e78ad7b3a831b3243409657d5e4ad
MouathSaadah <mouath.saadah@yahoo.com>
2022-03-11 18:36:55 +0300

    added unit test class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class Main {
---
+++ src/test/java/TestCases.java
@@ -0,0 +1,21 @@
+import com.neovisionaries.i18n.CountryCode;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestCases {
+
+    @Test
+    public void testDeal(){
+//        Deal deal = new Deal("Jordan1","Chinaa","1999");
+//        assertEquals(deal.getFromCurrency(), CountryCode.getByCode("Jordan"));
+//        assertNotEquals(deal.getFromCurrency(),CountryCode.getByCode("JOR"));
+//        assertNotNull(deal.getFromCurrency());
+//
+//        assertEquals(deal.getToCurrency(),"China");
+//        assertNotEquals(deal.getToCurrency(),"CHN");
+//        assertNotNull(deal.getToCurrency());
+//
+//        assertEquals(deal.getAmount(),1999);
+//        assertFalse(deal.getAmount()<0);
+
+    }
+}
